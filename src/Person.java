d96fe5b086974c9f4e733e5bf2ae5b0623ab7e00
Kyrmyzy Zholekesh <242247@astanait.edu.kz>
2024-12-26 16:19:18 +0500

    Assignment 1

---
+++ src/Person.java
@@ -0,0 +1,52 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private boolean gender;
+
+    public Person(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "\nHi I am " + name + " " + surname + "," +
+                "\na " + age + " year old" +
+                "\nGender: " + (gender ? "male" : "female");
+    }
+}
